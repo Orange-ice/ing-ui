f73bc812ffb596eb095d7abffe5a1fa9640f5043
burt <2902978956@qq.com>
2022-03-22 15:28:57 +0800

    修改 button 组件的单元测试

--- components/button/__tests__/button.unit.tsx
+++ components/button/__tests__/button.unit.tsx
@@ -12,13 +12,13 @@ describe('Button 组件测试', () => {
   it('props 测试', () => {
     const dom = renderer.create(<Button colorScheme="blue" variant="outline">Test</Button>);
     const instance = dom.root;
-    expect(instance.findByType(Button).props.effect).toBe('primary');
+    expect(instance.findByType(Button).props.colorScheme).toBe('blue');
     expect(instance.findByType(Button).props.variant).toBe('outline');
   });
 
   it('click 事件', () => {
     const fn = jest.fn();
-    const {container} = render(<Button colorScheme="blue" variant="outline" onClick={fn}>Test</Button>);
+    const {container} = render(<Button onClick={fn}>Test</Button>);
     fireEvent.click(container.querySelector('button') as HTMLButtonElement);
     expect(fn).toBeCalledTimes(1);
   });
